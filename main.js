3733f40994e50ea2a6c54f7cfa9858a768213e18
ClauYA <cyazoe5@gmail.com>
2026-09-08 12:22:37 -0400

    another exercise with strings

--- main.js
+++ main.js
@@ -84,3 +84,21 @@ function solution(str)
   }
 }
 console.log(solution('caden'))
+//Trolls are attacking your comment section!
+//A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+//For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+//Lets improve the time
+function disemvowel(str) 
+{
+  let nuevo=""
+  for(let i=0;i<str.length;i++)
+  {
+    if(str[i]!=='a' && str[i]!=='e' && str[i]!=='i' && str[i]!=='o' && str[i]!=='u' && str[i]!=='A' && str[i]!=='E' && str[i]!=='I' && str[i]!=='O' && str[i]!=='U')
+    {
+      nuevo+=str[i]
+    }
+  }
+  return nuevo
+}
+console.log(disemvowel("This website is for losers LOL!"))
